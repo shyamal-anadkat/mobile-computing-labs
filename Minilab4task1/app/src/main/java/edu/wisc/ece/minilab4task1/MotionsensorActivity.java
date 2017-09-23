06a2c423f628ad4642c0129ac3b22e00b510b93b
Shyamal Anadkat <shyamalanadkat@gmail.com>
2017-09-23 15:54:33 -0500

    Minilab4 Task 1, Acc. readings display

---
+++ Minilab4task1/app/src/main/java/edu/wisc/ece/minilab4task1/MotionsensorActivity.java
@@ -0,0 +1,123 @@
+package edu.wisc.ece.minilab4task1;
+
+import android.app.Activity;
+import android.graphics.Color;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MotionsensorActivity extends Activity implements SensorEventListener {
+    private SensorManager sensorManager;
+    private boolean color = false;
+    private View view;
+    private long lastUpdate;
+    TextView textx, texty, textz;
+
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(R.layout.activity_motionsensor);
+
+        // get textviews
+        textx = (TextView) findViewById(R.id.xval);
+        texty = (TextView) findViewById(R.id.yval);
+        textz = (TextView) findViewById(R.id.zval);
+
+
+        view = findViewById(R.id.textView);
+        view.setBackgroundColor(Color.BLUE);
+
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        sensorManager.registerListener(this,
+                sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),
+                SensorManager.SENSOR_DELAY_NORMAL);
+        lastUpdate = System.currentTimeMillis();
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+            displayAccelerometer(event);
+            checkShake(event);
+        }
+
+    }
+
+    private void displayAccelerometer(SensorEvent event) {
+
+        // Accelerometer sensor returns 3 values, one for each axis.
+        float[] acc_values = event.values;
+
+        // Movement values
+        float x = acc_values[0];
+        float y = acc_values[1];
+        float z = acc_values[2];
+
+        // display values using TextView
+        textx.setText("X Axis: "+ Float.toString(x));
+        texty.setText("Y Axis: "+ Float.toString(y));
+        textz.setText("Z Axis: "+ Float.toString(z));
+
+    }
+
+    private void checkShake(SensorEvent event) {
+
+        // Movement
+        float x = event.values[0];
+        float y = event.values[1];
+        float z = event.values[2];
+
+        float accelationSquareRoot = (x * x + y * y + z * z)
+                / (SensorManager.GRAVITY_EARTH * SensorManager.GRAVITY_EARTH);
+        long actualTime = System.currentTimeMillis();
+        if (accelationSquareRoot >= 2) //
+        {
+            if (actualTime - lastUpdate < 200) {
+                return;
+            }
+            lastUpdate = actualTime;
+            Toast.makeText(this, "Don't shake me!", Toast.LENGTH_SHORT).show();
+            if (color) {
+                view.setBackgroundColor(Color.BLUE);
+
+            } else {
+                view.setBackgroundColor(Color.RED);
+            }
+            color = !color;
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        // register this class as a listener for the orientation and
+        // accelerometer sensors
+        sensorManager.registerListener(this,
+                sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),
+                SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    protected void onPause() {
+        // unregister listener
+        super.onPause();
+        sensorManager.unregisterListener(this);
+    }
+}
